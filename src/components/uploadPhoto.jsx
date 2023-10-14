6683ab20a9c5773244f384e7e7e492bbe5dd374e
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2023-10-14 22:44:24 +0600

    Unknown Commit

--- src/components/uploadPhoto.jsx
+++ src/components/uploadPhoto.jsx
@@ -3,11 +3,11 @@ import axios from "axios";
 const img_hosting_token = import.meta.env.VITE_Image_Upload_Key;
 
 const uploadPhoto = async (file) => {
-    const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`;
-    const formData = new FormData();
-    formData.append("image", file);
+  const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`;
+  const formData = new FormData();
+  formData.append("image", file);
 
-   return await axios.post(img_hosting_url, formData)
-}
+  return await axios.post(img_hosting_url, formData);
+};
 
 export default uploadPhoto;
